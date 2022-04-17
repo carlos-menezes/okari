b00b9fba806d509db25ff8a1d114d158df340a5b
Carlos Menezes <carlos-menezes212@hotmail.com>
2022-04-17 14:07:36 +0100

    Generate `og` meta

--- src/converter.rs
+++ src/converter.rs
@@ -1,5 +1,5 @@
 use comrak::{markdown_to_html, ComrakOptions};
-use yaml_rust::YamlLoader;
+use yaml_rust::{Yaml, YamlLoader};
 
 pub struct Converter {}
 
@@ -13,14 +13,29 @@ impl Converter {
 <!DOCTYPE html>
 <html lang='en'>
     <head>
-        <title>{}</title>
+        {}
     </head>
     <body class='markdown-body'>
         {}
     </body>
 </html>",
-            frontmatter["title"].as_str().unwrap(),
+            Self::generate_head(frontmatter),
             content
         )
+    }
+
+    fn generate_head(frontmatter: &Yaml) -> String {
+        let title = frontmatter["title"]
+            .as_str()
+            .expect("`title` tag missing in frontmatter");
+        let description = frontmatter["description"]
+            .as_str()
+            .expect("`description` tag missing in frontmatter");
+
+        format!("
+            <title>{title}</title>
+            <meta property='og:title' content='{title}' />
+            <meta property='og:description' content='{description}' />
+            <meta property='og:image' content='https://og.railway.app/api/image?fileType=jpeg&layoutName=Pattern&Pattern=topography&Text={title}&Text+Color=%23ffffff&Foreground=%23808080&Background=%23000000&Opacity=0.5' />", title = title, description = description)
     }
 }
